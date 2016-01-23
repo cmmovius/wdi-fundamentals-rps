25cdfb36a01171a669d68f4f461c3ab8466fe2c7
Christine Movius <christine.movius@gmail.com>
2016-01-23 13:10:52 -0500

    added function getWinnter

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -29,13 +29,12 @@ function getPlayerMove(move) {
     } else if (move === "scissors") {
         return "scissors";
     } else if (move === null) {
-        return 'getInput()';
+        return function getInput();
     }
-}
-
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
+}
 
 function getComputerMove(move) {
     if (move === "rock") {
@@ -45,20 +44,39 @@ function getComputerMove(move) {
     } else if (move === "scissors") {
         return "scissors";
     } else if (move === null) {
-        return 'randomPlay()';
+        return function randomPlay();
     }
-}
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
+}
 
 function getWinner(playerMove,computerMove) {
     var winner;
+    if (playerMove === "rock" && computerMove === "rock") {
+        return "tie";
+    } else if (playerMove === "paper" && computerMove === "paper") {
+        return "tie";
+    } else if (playerMove === "scissors" && computerMove === "scissors") {
+        return "tie";
+    } else if (playerMove === "rock" && computerMove === "paper") {
+        return winner = "computer";
+    } else if (playerMove === "paper" && computerMove === "scissors") {
+        return winner = "computer";
+    } else if (playerMove === "scissors" && computerMove === "rock") {
+        return winner = "computer";
+    } else if (playerMove === "paper" && computerMove === "rock") {
+        return winner = "player";
+    } else if (playerMove === "scissors" && computerMove === "paper") {
+        return winner = "player";
+    } else if (playerMove === "rock" && computerMove === "scissors") {
+        return winner = "player";
+    } else {
+        return "error :-("
+    }
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
 }
 
 function playToFive() {
