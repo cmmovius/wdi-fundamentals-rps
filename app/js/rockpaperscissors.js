3f183d07a58c78b42df735b9c3c6f4893f63b7f5
Christine Movius <christine.movius@gmail.com>
2016-01-30 13:46:49 -0500

    modified playToFive function to use a while loop.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -68,7 +68,7 @@ function getWinner(playerMove,computerMove) {
     } else if (playerMove === "rock" && computerMove === "scissors") {
         return winner = "player";
     } else {
-        return "error :-(";
+        return "error getWinner";
     }
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
@@ -79,16 +79,20 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    var max = 5;
-    for (var i = playerWins || computerWins; i < max; i += 1)
-    if (getWinner === "player") {
-        return playerWins += 1
-    } else if (getWinner === "computer") {
-        return computerWins += 1
+    var winner = getWinner(getPlayerMove(), getComputerMove());
+    while ((playerWins < 4) && (computerWins < 4)) {
+        if (winner === "player") {
+            return playerWins += 1;
+        } else if (winner === "computer") {
+            return computerWins += 1;
+        }
+    } if (playerWins === 4) {
+        console.log ("Player Wins!");
+    } else if (computerWins === 4) {
+        console.log ("Computer Wins!");
     } else {
-        return "error :-(";
+        return "error playtoFive";
     }
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    //return [playerWins, computerWins];
+    // return [playerWins, computerWins]; I don't quite understand how to incorporate this.
 }
